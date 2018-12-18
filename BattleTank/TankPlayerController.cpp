51fd78252585efe1b68ee57fb8b550e06a77eefe
Seim <Seim@Home>
2018-12-18 07:30:51 +0100

    DeprojectScreenPositionToWOrld from CrossHairposition

--- BattleTank/TankPlayerController.cpp
+++ BattleTank/TankPlayerController.cpp
@@ -3,99 +3,99 @@
 #include "TankPlayerController.h"
 #include <DrawDebugHelpers.h>
 
-
-ATankPlayerController::ATankPlayerController()
-{
-	PrimaryActorTick.bCanEverTick = true;
+ATankPlayerController::ATankPlayerController() {
+  PrimaryActorTick.bCanEverTick = true;
 }
 
-void ATankPlayerController::BeginPlay()
-{
-	Super::BeginPlay();
+void ATankPlayerController::BeginPlay() {
+  Super::BeginPlay();
 
-
-	auto Tank = GetControlledTank();
-	
+  auto Tank = GetControlledTank();
 }
 
-void ATankPlayerController::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-	AimTowardsCrosshair();
+void ATankPlayerController::Tick(float DeltaTime) {
+  Super::Tick(DeltaTime);
+  AimTowardsCrosshair();
 }
 
-void ATankPlayerController::AimTowardsCrosshair()
-{
-	if (!GetControlledTank())
-	{
-		return;
-	}
+void ATankPlayerController::AimTowardsCrosshair() {
+  if (!GetControlledTank()) {
+    return;
+  }
 
-	FVector HitLocation; // Out parameter
+  FVector HitLocation; // Out parameter
 
-
-	if (GetSightRayHitLocation(HitLocation))
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Hit: %s"), *HitLocation.ToString());
-		// TODO tell controlled tank to aim at this point
-	}
-
-	
-
-	
-	
+  if (GetSightRayHitLocation(HitLocation)) {
+    UE_LOG(LogTemp, Warning, TEXT("Hit: %s"), *HitLocation.ToString());
+    // TODO tell controlled tank to aim at this point
+  }
 }
 
 // Get world location if linetrace through crosshair, true if hits landscape
-bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
-{
+bool ATankPlayerController::GetSightRayHitLocation(FVector &HitLocation) const {
 
-	// PseudoCode
-	// Find the CrosshairPosition
-	int32 ViewPortSizeX, ViewPortSizeY;
-	GetViewportSize(ViewPortSizeX, ViewPortSizeY);
-	// further: challenge lec 135: 4:56
+  // PseudoCode
+  // Find the CrosshairPosition in pixel coordinates
+  int32 ViewPortSizeX, ViewPortSizeY;
+  GetViewportSize(ViewPortSizeX, ViewPortSizeY);
+  // further: challenge lec 135: 4:56
 
-	// "De-project" the screen position of the crosshair to a world direction
-	// Line-trace along that look direction, and see what we hit (up to max range)
+  FVector2D ScreenLocation = FVector2D(CrossHairXLocation * ViewPortSizeX,
+                                       CrossHairYLocation * ViewPortSizeY);
 
-	FHitResult HitResult;
-	FVector StartTrace = FVector::ZeroVector;
-	if (auto Tank = GetControlledTank())
-	{
-		StartTrace = Tank->GetActorLocation();
-	}
-	// Position of the Middle of the screen, but with length X
-	float LengthCast = 100.f;
-	FVector EndTrace = FVector(960.f, 540.f, LengthCast);
-	FCollisionQueryParams QueryParams;
-	GetWorld()->LineTraceSingleByChannel(HitResult, StartTrace, EndTrace, ECollisionChannel::ECC_Visibility, QueryParams);
+  UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"),
+         *ScreenLocation.ToString());
 
-	DrawDebugLine(GetWorld(), StartTrace, EndTrace, FColor::Red, true, 3.f, 0, 2.f);
+  // "De-project" the screen position of the crosshair to a world direction
 
-	if (HitResult.bBlockingHit)
-	{
-		HitLocation = HitResult.ImpactPoint;
-		return true;
-	}
-	return false;
-	
+  FVector LookDirection;
+  if (GetLookDirection(ScreenLocation, LookDirection))
+  {
+	  UE_LOG(LogTemp, Warning, TEXT("WorldDirection: %s"), *LookDirection.ToString());
+  }
+
+  // Line-trace along that look direction, and see what we hit (up to max range)
+
+  //   FHitResult HitResult;
+  //   FVector StartTrace = FVector::ZeroVector;
+  //   if (auto Tank = GetControlledTank()) {
+  //     StartTrace = Tank->GetActorLocation();
+  //   }
+  //   // Position of the Middle of the screen, but with length X
+  //   float LengthCast = 100.f;
+  //   FVector EndTrace = FVector(960.f, 540.f, LengthCast);
+  //   FCollisionQueryParams QueryParams;
+  //   GetWorld()->LineTraceSingleByChannel(HitResult, StartTrace, EndTrace,
+  //                                        ECollisionChannel::ECC_Visibility,
+  //                                        QueryParams);
+  //
+  //   DrawDebugLine(GetWorld(), StartTrace, EndTrace, FColor::Red, true, 3.f,
+  //   0,
+  //                 2.f);
+  //
+  //   if (HitResult.bBlockingHit) {
+  //     HitLocation = HitResult.ImpactPoint;
+  //     return true;
+  //   }
+  return false;
 }
 
-ATank* ATankPlayerController::GetControlledTank() const
-{
-	auto Tank = Cast<ATank>(GetPawn());
-
-	if (Tank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("The Controlled Tank is: %s"), *GetPawn()->GetName());
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("No posseded Tank found."));
-	}
-
-	return Tank;
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation,
+                                             FVector &LookDirection) const {
+  // to be discarded
+  FVector CameraWorldLocation;
+  return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CameraWorldLocation, LookDirection);
 }
 
+ATank *ATankPlayerController::GetControlledTank() const {
+  auto Tank = Cast<ATank>(GetPawn());
 
+  if (Tank) {
+    UE_LOG(LogTemp, Warning, TEXT("The Controlled Tank is: %s"),
+           *GetPawn()->GetName());
+  } else {
+    UE_LOG(LogTemp, Warning, TEXT("No posseded Tank found."));
+  }
+
+  return Tank;
+}
--- BattleTank/TankPlayerController.h
+++ BattleTank/TankPlayerController.h
@@ -2,34 +2,39 @@
 
 #pragma once
 
-#include "Tank.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "Tank.h"
 #include "TankPlayerController.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
-class BATTLETANK_API ATankPlayerController : public APlayerController
-{
-	GENERATED_BODY()
-	
+class BATTLETANK_API ATankPlayerController : public APlayerController {
+  GENERATED_BODY()
 
 public:
+  ATankPlayerController();
 
-	ATankPlayerController();
+  ATank *GetControlledTank() const;
 
-	ATank* GetControlledTank() const;
-	
-	void BeginPlay() override;
+  void BeginPlay() override;
 
-	void Tick(float DeltaTime) override;
+  void Tick(float DeltaTime) override;
 
 private:
-	// start the tank moving the barrel so that the shot would hit what the crosshair would hit
-	void AimTowardsCrosshair();
+  // start the tank moving the barrel so that the shot would hit what the
+  // crosshair would hit
+  void AimTowardsCrosshair();
 
-	bool GetSightRayHitLocation(FVector& HitLocation) const;
-	
+  bool GetSightRayHitLocation(FVector &HitLocation) const;
+
+  bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+  UPROPERTY(EditAnywhere, Category = "Aiming")
+  float CrossHairXLocation = 0.5f;
+
+  UPROPERTY(EditAnywhere, Category = "Aiming")
+  float CrossHairYLocation = 0.3333333f;
 };
